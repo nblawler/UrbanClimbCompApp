4a857bb4a585ca725c9a4a6e7089ce7dc7da033d
Nick Lawler <s3951510@student.rmit.edu.au>
2026-02-20 08:44:05 +1100

    added admin and api blueprint

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,9 +2,13 @@ from .auth import auth_bp
 from .competitions import comp_bp
 from .login import login_bp
 from .competitors import competitors_bp
+from .admin import admin_bp
+from .api import api_bp
 
 def register_blueprints(app):
     app.register_blueprint(auth_bp)
     app.register_blueprint(comp_bp)
     app.register_blueprint(login_bp)
     app.register_blueprint(competitors_bp)
+    app.register_blueprint(admin_bp)
+    app.register_blueprint(api_bp)
