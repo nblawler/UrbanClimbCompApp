1dad013f40584b6a353d6c6a44a6166eae19c63d
Nick Lawler <s3951510@student.rmit.edu.au>
2026-02-24 09:22:14 +1100

    add missing route

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,6 +4,7 @@ from .auth import auth_bp
 from .competitions import competitions_bp
 from .competitors import competitors_bp
 from .scores import scores_bp
+from .climbs import climbs_bp
 
 def register_blueprints(app):
     app.register_blueprint(index_bp)
@@ -12,3 +13,4 @@ def register_blueprints(app):
     app.register_blueprint(competitions_bp)
     app.register_blueprint(competitors_bp)
     app.register_blueprint(scores_bp)
+    app.register_blueprint(climbs_bp)
